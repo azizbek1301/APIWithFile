3d30818e765ae4f3b7760bbeda1b1c47f0a6bcf3
azizbek1301 <azizbekrakhmatullayev1317@gmail.com>
2023-11-22 11:43:02 +0500

    Add project files.

---
+++ FileAPI/Data/ProfileDbContext.cs
@@ -0,0 +1,14 @@
+﻿using FileAPI.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace FileAPI.Data
+{
+    public class ProfileDbContext:DbContext
+    {
+        public ProfileDbContext(DbContextOptions<ProfileDbContext> options):base(options)
+        {
+            
+        }
+        public virtual DbSet<ProfileTeacher> Teachers { get; set; }
+    }
+}
---
+++ FileAPI/Entities/ProfileTeacher.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Text.Json.Serialization;
+
+namespace FileAPI.Entities
+{
+    public class ProfileTeacher
+    {
+        // jsonga chiqarmedi
+        [JsonIgnore]
+        public Guid Id { get; set; }
+        [Required]
+        public string Name { get; set; } = default!;
+
+        // Bu jsonga ko'rsatilmedi ko'rinmedi
+        [JsonIgnore]
+        public string ImageUrl {  get; set; }
+
+        // Bu bazaga qo'shilmedi
+        [NotMapped]
+        public IFormFile Image { get; set; }
+    }
+}
